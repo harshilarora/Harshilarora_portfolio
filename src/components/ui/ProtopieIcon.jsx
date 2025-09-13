0b6cdd11b22730fdbd689c62c0ebe939262ab1f6
HARSHIL ARORA <harshilarora0@gmail.com>
2025-09-13 02:48:48 -0700

    Deploy site

--- src/components/ui/ProtopieIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import protopieLogo from "../../assets/Images/protopie2.svg";
-
-const ProtopieIcon = ({ className = "w-24 h-auto" }) => {
-  return <img src={protopieLogo} alt="ProtoPie Logo" className={className} />;
-};
-
-export default ProtopieIcon;
--- src/components/ui/RhinoIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import rhinoIcon1 from "../../assets/Images/rhinoIcon1.png";
-
-const ProtopieIcon = ({ className = "w-14 h-auto" }) => {
-  return <img src={rhinoIcon1} alt="ProtoPie Logo" className={className} />;
-};
-
-export default ProtopieIcon;
